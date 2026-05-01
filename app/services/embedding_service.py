3bb011a9413a9960d32bbab46ec6830d1112462b
Samarth <samarthsr04@gmail.com>
2026-05-01 15:56:46 +0530

    cosine logic fix

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,18 +1,15 @@
-import json
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-from app.models import resume
-from app.models import job
-
-
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
 
 def generate_embedding(text: str):
     embedding = model.encode(text)
     return embedding.tolist()
 
+
 def cosine_similarity(vec1, vec2):
-    v1 = np.array(json.loads(job.embedding))
-    v2 = np.array(json.loads(resume.embedding))
+    v1 = np.array(vec1)
+    v2 = np.array(vec2)
     return float(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
